c7c9c38d56bce58d6b60ab704abca92a35bc9078
kevinWalshMunoz <pkevin.walsh@gmail.com>
2024-08-27 13:10:48 -0600

    chore: Add dotenv dependency and configure server port

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require("express");
 const path = require("path");
 const http = require("http");
@@ -70,8 +72,10 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = server;
